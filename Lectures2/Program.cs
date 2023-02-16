67d6cc7ca8867e1e17c66a023344e80f219be92a
Стасян <stas.baranovfm@gmail.com>
2023-02-16 23:30:21 +0300

    preservation2

--- Lectures2/Program.cs
+++ Lectures2/Program.cs
@@ -77,16 +77,71 @@
 
 // Поиск позиции нужного элемента массива
 //              0   1   2  3   4   5   6   7   8          индексы
-int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
-int n = array.Length;
-int find = 18;
-int index = 0;
-while (index < n)
+// int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
+// int n = array.Length;
+// int find = 18;
+// int index = 0;
+// while (index < n)
+// {
+//     if (array[index] == find)
+//     {
+//         Console.WriteLine(index);
+//         break;  // если в массиве есть несколько одинаковых элементов, то поиск прекращается при нахождении искомого элемента
+//     }
+//     index++;
+// }
+// _________________________________________________________________________________________________
+
+// Поиск позиции нужного элемента массива с использованием генератора случайных чисел и функции
+
+void FillArray(int[] collection)    // FillArray - наименование метода (любое),collection - название аргумента, void - метод который ничего не возвращает (return не нужен)
 {
-    if (array[index] == find)
+    int length = collection.Length;
+    int index = 0; 
+    while (index < length)
     {
-        Console.WriteLine(index);
-        break;                          // если в массиве есть несколько одинаковых элементов, то поиск прекращается при нахождении первого одинакового элемента
+        collection[index] = new Random().Next(1, 10);
+        index++;
     }
-    index++;
 }
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0; 
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+            // int position = -1; // в случае если ищем позицию элемента больше чем сам массив, то вместо 0 пишем -1 (*)
+    
+
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // создание массива с 10-тью ячейками
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array,4);
+        //int pos = IndexOf(array,444);// в случае если ищем позицию элемента не 4 а 444 (*)
+Console.WriteLine(pos);
